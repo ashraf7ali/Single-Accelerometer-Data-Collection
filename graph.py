814e6b26f0b95c32005d6c899cce62bfaee4e1ab
Mir Ashraf Ali <49491752+ashraf7ali@users.noreply.github.com>
2022-08-10 01:51:06 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('QuickMill_out.csv')
+df = df.set_index("Timestamp")
 
 df.plot()
-# df.plot(kind = 'scatter', x = 'Timestamp', y = 'Aplitude')
+dtFmt = mdates.DateFormatter('%Y-%m-%d') # define the formatting
+plt.gca().xaxis.set_major_formatter(dtFmt)
+plt.xticks(rotation = 45)
 plt.show()
-print(df.head()) 
