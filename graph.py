766e44acde3e46bb2c3043de2e9b93c230d07fdb
Mir Ashraf Ali <49491752+ashraf7ali@users.noreply.github.com>
2022-08-11 13:37:11 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import matplotlib.dates as mdates
 
 df = pd.read_csv('QuickMill_out.csv')
+df["Timestamp"] = pd.to_datetime(df["Timestamp"])
 df = df.set_index("Timestamp")
-
 df.plot()
-dtFmt = mdates.DateFormatter('%Y-%m-%d') # define the formatting
+dtFmt = mdates.DateFormatter("%H:%M:%S") # define the formatting
 plt.gca().xaxis.set_major_formatter(dtFmt)
 plt.xticks(rotation = 45)
+plt.ylabel("Amplitude")
+plt.xlabel("Time")
 plt.show()
+
